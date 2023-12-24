8e34451f43e005e27c20df573ea8e5cfbac8e62e
ozzyverse <58913474+ozzyverse@users.noreply.github.com>
2023-12-24 10:08:05 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./Navbar";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center">Hello Vite + React!</h1>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,19 @@
+export default function Navbar() {
+  return (
+    <>
+      <nav>
+        <ul className="flex justify-around">
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Products</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
